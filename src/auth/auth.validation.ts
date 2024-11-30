6dc68679d7fa3f18e5689c26204eeda4b0e6fdba
r3Vibe <arnab95gupta@gmail.com>
2024-11-30 14:51:15 +0530

    update

--- src/auth/auth.validation.ts
+++ src/auth/auth.validation.ts
@@ -1,27 +1,12 @@
 import * as Joi from 'joi';
-import {
-  objectId,
-  phoneValidation,
-} from 'src/common/validation/custom.validation';
+import { i18nValidationMessage } from 'nestjs-i18n';
 
 export const CreateUserEmailPassValidation = Joi.object({
-  first_name: Joi.string().allow('').required().label('First Name'),
-  last_name: Joi.string().optional().allow('').label('Last Name'),
-  email: Joi.string().email().required().label('Email'),
-  phone: Joi.string()
-    .required()
+  first_name: Joi.string()
     .allow('')
-    .label('Phone')
-    .custom(phoneValidation),
-  password: Joi.string().required().label('Password'),
-  id: Joi.string().required().custom(objectId),
-  my: Joi.array()
-    .items(
-      Joi.object({
-        id: Joi.string().required().custom(objectId),
-      }),
-    )
     .required()
-    .min(2)
-    .max(5),
+    .label('First Name')
+    .messages({
+      'string.empty': i18nValidationMessage('error.empty') as any,
+    }),
 });
